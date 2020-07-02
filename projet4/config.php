6974cb9d1c66ed2bb68cc0c03cf99e24f224565c
Simon <simon.morvant@yahoo.com>
2020-07-02 12:28:07 -0300

    test push for ignore

--- projet4/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$nomDeDomaine = 'http://dromptar.com/projet4/index.php';
-
-$connectionString = 'mysql:host=dromptarvzdrom.mysql.db;port=3306;dbname=dromptarvzdrom;charset=utf8';
-$dbUser = 'dromptarvzdrom';
-$dbPassword = 'celeDromptar1492';
-
-?>
